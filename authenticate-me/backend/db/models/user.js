4b17edc091ca9bc58b8098a7eee2531e73c7b0ec
Pete Franco <petethe4th@live.com>
2022-12-17 14:27:24 -0600

    Add User Signup backend endpoint

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -1,9 +1,7 @@
-'use strict';
+"use strict";
 
-const bcrypt = require('bcryptjs');
-const {
-  Model
-} = require('sequelize');
+const bcrypt = require("bcryptjs");
+const { Model, Validator } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
@@ -20,17 +18,17 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static async login({ credential, password }) {
-      const { Op } = require('sequelize');
-      const user = await User.scope('loginUser').findOne({
+      const { Op } = require("sequelize");
+      const user = await User.scope("loginUser").findOne({
         where: {
           [Op.or]: {
             username: credential,
-            email: credential
-          }
-        }
+            email: credential,
+          },
+        },
       });
       if (user && user.validatePassword(password)) {
-        return await User.scope('currentUser').findByPk(user.id);
+        return await User.scope("currentUser").findByPk(user.id);
       }
     }
 
@@ -39,9 +37,9 @@ module.exports = (sequelize, DataTypes) => {
       const user = await User.create({
         username,
         email,
-        hashedPassword
+        hashedPassword,
       });
-      return await User.scope('currentUser').findByPk(user.id);
+      return await User.scope("currentUser").findByPk(user.id);
     }
 
     /**
@@ -53,60 +51,63 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [4,30],
-        isNotEmail(value) {
-          if(validator.isEmail(value)) {
-            throw new Error("Cannot be an email.")
-          }
-        }
-      }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        len: [60, 60],
+      },
     },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3,256],
-        isEmail: true
-      }
-    },
-    hashedPassword: {
-      type: DataTypes.STRING.BINARY,
-      allowNull: false,
-      len: [60,60]
-    }
-  }, {
-    sequelize,
-    modelName: 'User',
-    defaultScope: {
-      attributes: {
-        exclude: ["hashedPassword", "email", "createdAt","updatedAt"]
-      }
-    },
-    scopes: {
-      currentUser: {
+    {
+      sequelize,
+      modelName: "User",
+      defaultScope: {
         attributes: {
-          exclude: ["hashedPassword"]
-        }
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+        },
       },
-      loginUser: {
-        attributes: {}
-      }
-    },
-    indexes: [
-      {
-        fields:['username'],
-        unique: true
+      scopes: {
+        currentUser: {
+          attributes: {
+            exclude: ["hashedPassword"],
+          },
+        },
+        loginUser: {
+          attributes: {},
+        },
       },
-      {
-        fields: ['email'],
-        unique: true
-      }
-    ]
-  });
+      indexes: [
+        {
+          fields: ["username"],
+          unique: true,
+        },
+        {
+          fields: ["email"],
+          unique: true,
+        },
+      ],
+    }
+  );
   return User;
 };
--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -1,5 +1,20 @@
 // backend/routes/api/users.js
-const express = require('express')
+const express = require("express");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
 const router = express.Router();
+
+// Sign up
+router.post("/", async (req, res) => {
+  const { username, email, password} = req.body;
+  const user = await User.signup({ username, email, password });
+
+  await setTokenCookie(res, user);
+
+  return res.json({
+    user: user,
+  });
+});
 
 module.exports = router;
