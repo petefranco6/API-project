5afba042e602931f9705c7c78db13e9665d506ee
Pete Franco <petethe4th@live.com>
2023-01-23 17:32:00 -0600

    added seed data

--- backend/db/seeders/20221217051727-demo-user.js
+++ backend/db/seeders/20221217051727-demo-user.js
@@ -36,7 +36,7 @@ module.exports = {
       lastName: "User1",
       email: 'user1@user.io',
       username: 'FakeUser1',
-      hashedPassword: bcrypt.hashSync('password2'),
+      hashedPassword: bcrypt.hashSync('password'),
       roleId: 1
     },
     {
@@ -44,7 +44,31 @@ module.exports = {
       lastName: "User2",
       email: 'user2@user.io',
       username: 'FakeUser2',
-      hashedPassword: bcrypt.hashSync('password3'),
+      hashedPassword: bcrypt.hashSync('password'),
+      roleId: 2
+    },
+    {
+      firstName: "Fake",
+      lastName: "user3",
+      email: 'user3@user.io',
+      username: 'FakeUser3',
+      hashedPassword: bcrypt.hashSync('password'),
+      roleId: 1
+    },
+    {
+      firstName: "Fake",
+      lastName: "User4",
+      email: 'user4@user.io',
+      username: 'FakeUser4',
+      hashedPassword: bcrypt.hashSync('password'),
+      roleId: 1
+    },
+    {
+      firstName: "Fake",
+      lastName: "User5",
+      email: 'user5@user.io',
+      username: 'FakeUser5',
+      hashedPassword: bcrypt.hashSync('password'),
       roleId: 2
     }
    ], {});
